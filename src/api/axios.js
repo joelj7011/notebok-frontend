b4e0b845534023e08123b0e718e66feef3e4a867
Shivang Tiwari <93948068+joelj7011@users.noreply.github.com>
2024-07-07 15:42:18 +0530

    Update axios.js
    changes frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://notebook-backend-cejg.onrender.com';
+const BASE_URL = 'http://localhost:3000';
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
